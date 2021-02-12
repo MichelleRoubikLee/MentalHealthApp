0dc423d5243f5f221013339f611cf498a503181b
MichelleRoubikLee <michelleroubiklee@gmail.com>
2021-02-11 22:28:19 -0600

    daily log checks that logs are at least 12 hours apart before bringing up question cards again

--- client/src/components/dayLog/dayLog.jsx
+++ client/src/components/dayLog/dayLog.jsx
@@ -7,9 +7,15 @@ import useFirstRender from "../../firstRenderHook/useFirstRender";
 //import jwt_decode from "jwt-decode";
 
 function DayLog(props) {
-    const [zipCode, setZipCode] = useState("")
+    const [zipCode, setZipCode] = useState("");
+    let logTime = 0;
+    let questions = [];
+    let weatherQuestions = [];
+
     const firstRender = useFirstRender();
-    var date = new Date().toISOString()
+
+    var date = new Date().toISOString();
+    
     var token = sessionStorage.getItem('sessionId');
     //var decoded = jwt_decode(token);
 
@@ -17,20 +23,40 @@ function DayLog(props) {
         setZipCode(event.target.value)
     }
 
+    function compareDates(element,array){
+        if(element.logs.length != 0){
+
+            logTime = element.logs[element.logs.length -1].date;
+            const logYear = parseInt(logTime.slice(0,4));
+            const logMonth = parseInt(logTime.slice(5,7));
+            const logDay = parseInt(logTime.slice(8,10));
+            const logHour = parseInt(logTime.slice(11,13));
+
+            const logTotalHours = (logYear * 8760) + (logMonth * 730) + (logDay * 12) + logHour;
+
+            const nowYear = parseInt(date.slice(0,4));
+            const nowMonth = parseInt(date.slice(5,7));
+            const nowDay = parseInt(date.slice(8,10));
+            const nowHour = parseInt(date.slice(11,13));
+
+            const nowTotalHours = (nowYear * 8760) + (nowMonth * 730) + (nowDay * 12) + nowHour;
+            console.log("log: " + logTotalHours);
+            console.log("now: " + nowTotalHours);
+            if(nowTotalHours - logTotalHours >= 12){
+                array.push(element);
+            }
+        }
+    }
+
     function showQuestions(){
         if(token && props.userData.factors != 0){
-            //now date minus log date, if > 12, show card
             props.userData.factors.forEach(element => {
-                if(element.logs.length == 0){
-                    console.log(element.logs.length)
-                }else{
-                    console.log(element.logs[0].date);
-                    console.log(date);
-                }
+                compareDates(element,questions);
             });
+
             return(
                 <div>
-                    {props.userData.factors.map((oneFactor, index) => (
+                    {questions.map((oneFactor, index) => (
                         <QuestionCard key={index} factor={oneFactor} getUser = {props.getUser}/>
                     ))} 
                 </div>
@@ -40,9 +66,12 @@ function DayLog(props) {
     
     function showWeatherQuestions(){
         if(!firstRender && token && props.userData.factors != 0){
+            props.userData.weatherFactors.forEach(element => {
+                compareDates(element,weatherQuestions);
+            });
             return(
                 <div>
-                    {props.userData.weatherFactors.map((oneFactor, index) => (
+                    {weatherQuestions.map((oneFactor, index) => (
                         <WeatherCard key={index} factor={oneFactor} getUser = {props.getUser} userData={props.userData} zipCode={zipCode}/>
                     ))}
                 </div>
